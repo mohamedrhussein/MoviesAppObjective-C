36e66a46fb1d601272680455331b75d69b69fab4
Mohamed Rabie <mohamed.rabie.hussein@gmail.com>
2022-11-08 23:26:05 +0200

    add Details Files

---
+++ MoviesApp/Details/DetailsViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailsViewController.h
+//  MoviesApp
+//
+//  Created by Mohamed Rabie on 11/8/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
